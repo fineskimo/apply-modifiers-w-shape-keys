2607508152b3a21dd1cbc229856da81fa652edaf
fineskimo <fineskimo@users.noreply.github.com>
2020-09-19 20:56:43 +0100

    add multi modifier apply
    Operator now prompts for modifier selection

--- __init__.py
+++ __init__.py
@@ -34,87 +34,8 @@ def ShowMessageBox(message="", title="Message Box", icon='INFO'):
     bpy.context.window_manager.popup_menu(draw, title=title, icon=icon)
 
 
-def apply_modifiers(context, modifierName):
-    list_names = []
-    list = []
-    list_shapes = []
-    basename = bpy.context.object.name
-
-    if context.object.data.shape_keys:
-        list_shapes = [o for o in context.object.data.shape_keys.key_blocks]
-
-    if(list_shapes == []):
-        bpy.ops.object.modifier_apply(modifier=modifierName)
-        return context.view_layer.objects.active
-
-    list.append(context.view_layer.objects.active)
-
-    for i in range(1, len(list_shapes)):
-        bpy.ops.object.duplicate(linked=False, mode='TRANSLATION')
-
-        list.append(context.view_layer.objects.active)
-
-    for i, o in enumerate(list):
-        context.view_layer.objects.active = o
-        list_names.append(o.data.shape_keys.key_blocks[i].name)
-
-        for j in range(i + 1, len(list))[::-1]:
-            context.object.active_shape_key_index = j
-            print("loop one")
-
-            bpy.ops.object.shape_key_remove()
-
-        for j in range(0, i):
-            print("loop two")
-            context.object.active_shape_key_index = 0
-
-            bpy.ops.object.shape_key_remove()
-
-        # last deleted shape doesn't change object shape
-        context.object.active_shape_key_index = 0
-        bpy.ops.object.shape_key_remove()
-        print("out")
-
-        try:
-            bpy.ops.object.modifier_apply(modifier=modifierName)
-            print("apply")
-        except RuntimeError:
-            ShowMessageBox("Modifer is disabled, skipping apply", "Report: Error ", 'ERROR')
-
-        if i > 0:
-            bpy.context.object.name = f'{basename}_{list_names[i]}'
-            # time to apply modifiers
-            try:
-                bpy.ops.object.modifier_apply(modifier=modifierName)
-                print("apply2")
-            except RuntimeError:
-                ShowMessageBox("Modifer is disabled, skipping apply", "Report: Error ", 'ERROR')
-
-    bpy.ops.object.select_all(action='DESELECT')
-    context.view_layer.objects.active = list[0]
-    list[0].select_set(True)
-    bpy.ops.object.shape_key_add(from_mix=False)
-    context.object.data.shape_keys.key_blocks[0].name = list_names[0]
-
-    for i in range(1, len(list)):
-        list[i].select_set(state=True)
-        bpy.ops.object.join_shapes()
-        list[i].select_set(state=False)
-        context.object.data.shape_keys.key_blocks[i].name = list_names[i]
-
-    bpy.ops.object.select_all(action='DESELECT')
-
-    for o in list[1:]:
-        o.select_set(True)
-
-    bpy.ops.object.delete(use_global=False)
-    context.view_layer.objects.active = list[0]
-    context.view_layer.objects.active.select_set(state=True)
-    return context.view_layer.objects.active
-
-
-class AWS_OT_operator(bpy.types.Operator):
-    bl_idname = "aws.operator"
+class AWS_OT_applymodswithshapekeys(bpy.types.Operator):
+    bl_idname = "aws.applymodswithshapekeys"
     bl_label = "Apply Modifiers w/Shape Keys"
     bl_description = ("Apply Modifiers for objects with Shape Keys")
     bl_options = {'UNDO'}
@@ -126,17 +47,20 @@ class AWS_OT_operator(bpy.types.Operator):
     my_enum: EnumProperty(name="Modifier name",
                           items=item_list)
 
-    # TODO add muilti modifier apply support
-    # selection: BoolVectorProperty(
-    #     size=32,
-    #    options={'SKIP_SAVE'}
-    # )
+    selection: BoolVectorProperty(
+        size=32,
+        options={'SKIP_SAVE'}
+        )
 
-    # def draw(self, context):
-    #    layout = self.layout
-    #    for idx, mod in enumerate(context.active_object.modifiers):
-    #        layout.prop(self, 'selection', index=idx, text=mod.name,
-    #                    toggle=True)
+    def draw(self, context):
+        layout = self.layout
+        for idx, mod in enumerate(context.active_object.modifiers):
+            layout.prop(self, 'selection', index=idx, text=mod.name, toggle=True)
+
+    def invoke(self, context, event):
+        wm = context.window_manager
+        wm.invoke_props_dialog(self)
+        return {'RUNNING_MODAL'}
 
     def execute(self, context):
 
@@ -145,35 +69,105 @@ class AWS_OT_operator(bpy.types.Operator):
         ob.select_set(True)
         context.view_layer.objects.active = ob
 
-        # not great code ()
+        # not great code (switching area type temporarily to avoid context errors)
         area = bpy.context.area
-        old_type = area.type
+        active_type = area.type
         area.type = 'VIEW_3D'
 
-        apply_modifiers(context, self.my_enum)
+        list_names = []
+        obj_list = []
+        list_shapes = []
+        basename = bpy.context.object.name
 
-        area.type = old_type
+        if context.object.data.shape_keys:
+            list_shapes = [o for o in context.object.data.shape_keys.key_blocks]
+
+        if(list_shapes == []):
+            j = 31
+            for i, flag in reversed(list(enumerate(reversed(self.selection)))):
+                if flag:
+                    modifierName = ob.modifiers[j - i].name
+                    bpy.ops.object.modifier_apply(modifier=modifierName)
+                    j -= 1
+
+            area.type = active_type
+            return {'FINISHED'}
+
+        obj_list.append(context.view_layer.objects.active)
+
+        for i in range(1, len(list_shapes)):
+            bpy.ops.object.duplicate(linked=False, mode='TRANSLATION')
+
+            obj_list.append(context.view_layer.objects.active)
+
+        for i, o in enumerate(obj_list):
+            context.view_layer.objects.active = o
+            list_names.append(o.data.shape_keys.key_blocks[i].name)
+
+            for j in range(i + 1, len(obj_list))[::-1]:
+                context.object.active_shape_key_index = j
+
+                bpy.ops.object.shape_key_remove()
+
+            for j in range(0, i):
+                context.object.active_shape_key_index = 0
+
+                bpy.ops.object.shape_key_remove()
+
+            # last deleted shape doesn't change object shape
+            context.object.active_shape_key_index = 0
+            bpy.ops.object.shape_key_remove()
+
+            try:
+                j = 31
+                for i, flag in reversed(list(enumerate(reversed(self.selection)))):
+                    if flag:
+                        modifierName = o.modifiers[j - i].name
+                        bpy.ops.object.modifier_apply(modifier=modifierName)
+                        j -= 1
+
+            except RuntimeError:
+                area.type = active_type
+                ShowMessageBox("Modifer is disabled, skipping apply", "Report: Error ", 'ERROR')
+
+            if i > 0:
+                bpy.context.object.name = f'{basename}_{list_names[i]}'
+                # time to apply modifiers
+                try:
+                    j = 31
+                    for i, flag in reversed(list(enumerate(reversed(self.selection)))):
+                        if flag:
+                            modifierName = o.modifiers[j - i].name
+                            bpy.ops.object.modifier_apply(modifier=modifierName)
+                            j -= 1
+                except RuntimeError:
+                    area.type = active_type
+                    ShowMessageBox("Modifer is disabled, skipping apply", "Report: Error ", 'ERROR')
+
+        bpy.ops.object.select_all(action='DESELECT')
+        context.view_layer.objects.active = obj_list[0]
+        obj_list[0].select_set(True)
+        bpy.ops.object.shape_key_add(from_mix=False)
+        context.object.data.shape_keys.key_blocks[0].name = list_names[0]
+
+        for i in range(1, len(obj_list)):
+            obj_list[i].select_set(state=True)
+            bpy.ops.object.join_shapes()
+            obj_list[i].select_set(state=False)
+            context.object.data.shape_keys.key_blocks[i].name = list_names[i]
+
+        bpy.ops.object.select_all(action='DESELECT')
+
+        for o in obj_list[1:]:
+            o.select_set(True)
+
+        bpy.ops.object.delete(use_global=False)
+        context.view_layer.objects.active = obj_list[0]
+        context.view_layer.objects.active.select_set(state=True)
+
+        area.type = active_type
 
         return {'FINISHED'}
-
-    def invoke(self, context, event):
-        return context.window_manager.invoke_props_dialog(self)
-
-
-def register():
-    from bpy.utils import register_class
-    for cls in classes:
-        register_class(cls)
-
-
-def unregister():
-    from bpy.utils import unregister_class
-    for cls in reversed(classes):
-        unregister_class(cls)
-
-
-if __name__ == "__main__":
-    register()
 
 
 # Menus #
@@ -182,7 +176,7 @@ def menu(self, context):
         if (len(context.active_object.modifiers)):
             layout = self.layout
             row = layout.row()
-            row.operator(AWS_OT_operator.bl_idname,
+            row.operator(AWS_OT_applymodswithshapekeys.bl_idname,
                          icon='SHAPEKEY_DATA',
                          text="Apply Modifiers w/Shapekeys")
 
@@ -192,7 +186,7 @@ def menu_func(self, context):
         if (len(context.active_object.modifiers)):
             layout = self.layout
             layout.separator()
-            layout.operator(AWS_OT_operator.bl_idname,
+            layout.operator(AWS_OT_applymodswithshapekeys.bl_idname,
                             icon='SHAPEKEY_DATA',
                             text="Apply Modifiers w/Shapekeys")
 
@@ -200,7 +194,7 @@ def menu_func(self, context):
 def register():
     from bpy.utils import register_class
 
-    register_class(AWS_OT_operator)
+    register_class(AWS_OT_applymodswithshapekeys)
 
     # Add "Specials" menu to the "Modifiers" menu
     bpy.types.DATA_PT_modifiers.prepend(menu)
@@ -217,7 +211,7 @@ def unregister():
     # bpy.types.VIEW3D_MT_object_apply.remove(menu_func)
 
     from bpy.utils import unregister_class
-    unregister_class(AWS_OT_operator)
+    unregister_class(AWS_OT_applymodswithshapekeys)
 
 
 if __name__ == "__main__":
